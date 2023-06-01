151de7aba193d9af6a93d1fa9deaca9a7e7827fa
jonatao37 <jonathan.almeida@sptech.school>
2023-06-01 15:41:39 -0300

    Quiz contruido

--- Site institucional/web-data-viz-main/site/public/Itens_site/quiz.js
+++ Site institucional/web-data-viz-main/site/public/Itens_site/quiz.js
@@ -1,206 +1,73 @@
 b_usuario.innerHTML = sessionStorage.NOME_USUARIO;
 
-var miles = 0
-var gwen = 0
-var peter = 0
-var noir = 0
-var peni = 0
-var porker = 0
 
-var Resposta1 = resposta1.value
-var Resposta2 = resposta2.value
-var Resposta3 = resposta3.value
-var Resposta4 = resposta4.value
-var Resposta5 = resposta5.value
-var Resposta6 = resposta6.value
-var Resposta7 = resposta7.value
-var Resposta8 = resposta8.value
-var Resposta9 = resposta9.value
-var Resposta10 = resposta10.value
-var Resposta11 = resposta11.value
-var Resposta12 = resposta12.value
-var Resposta13 = resposta13.value
-var Resposta14 = resposta14.value
-var Resposta15 = resposta15.value
-var Resposta16 = resposta16.value
-var Resposta17 = resposta17.value
-var Resposta18 = resposta18.value
-var Resposta19 = resposta19.value
-var Resposta20 = resposta20.value
-var Resposta21 = resposta21.value
-var Resposta22 = resposta22.value
-var Resposta23 = resposta23.value
-var Resposta24 = resposta24.value
-var Resposta25 = resposta25.value
-var Resposta26 = resposta26.value
-var Resposta27 = resposta27.value
-var Resposta28 = resposta28.value
-var Resposta29 = resposta29.value
-var Resposta30 = resposta30.value
-var Resposta31 = resposta31.value
-var Resposta32 = resposta32.value
-var Resposta33 = resposta33.value
-var Resposta34 = resposta34.value
-var Resposta35 = resposta35.value
-var Resposta36 = resposta36.value
+
+function responder (pergunta, resposta) {
+    if (resposta == 1) {
+
+        var valorMiles = Number(localStorage.getItem('miles'));
+        valorMiles++;
+        localStorage.setItem('miles', valorMiles);
+        
+    } else if (resposta == 2) {
+
+        var valorGwen = Number(localStorage.getItem('gwen'));
+        valorGwen++;
+        localStorage.setItem('gwen', valorGwen);
+        
+
+    }else if (resposta == 3) {
+
+        var valorPeter = Number(localStorage.getItem('peter'));
+        valorPeter++;
+        localStorage.setItem('peter', valorPeter);
+       
+
+    }else if (resposta == 4) {
+
+        var valorPeni = Number(localStorage.getItem('peni'));
+        valorPeni++;
+        localStorage.setItem('peni', valorPeni);
+        
+
+    }else if (resposta == 5) {
+
+        var valorNoir = Number(localStorage.getItem('noir'));
+        valorNoir++;
+        localStorage.setItem('noir', valorNoir);
+        
+
+    }else {
+
+        var valorPorker = Number(localStorage.getItem('porker'));
+        valorPorker++;
+        localStorage.setItem('porker', valorPorker);
+        
+
+    }
+    
+    if(pergunta < 6){
+    var pagina = `pergunta${pergunta + 1}.html`;
+    window.location.href = pagina;
+
+}else {
+
+    var resultado = `resultado.html`;
+    window.location = resultado;
+
+}
+
+}
 
 function quiz() {
 
-    for (var contador = 1; contador > 6; contador++) {
 
-        if (contador == 1) {
-
-            if (Resposta1 == "Miles Morales") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta2) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta3) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta4) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta5) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-        if (contador == 2) {
-
-            if (Resposta7 == "Sunflower - Post Malone") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta12) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta9) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta8) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta10) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-        if (contador == 3) {
-
-            if (Resposta14 == "Hamburguer") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta18) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta15) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta16) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta17) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-        if (contador == 4) {
-
-            if (Resposta22 == "Determinado e Leal") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta20) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta19) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta23) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta24) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-        if (contador == 5) {
-
-            if (Resposta26 == "Vermelho") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta25) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta28) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta27) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta29) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-        if (contador == 6) {
-
-            if (Resposta32 == "Humildade e Empatia") {
-                var valorMiles = Number(localStorage.getItem('miles'));
-                localStorage.setItem("miles", valorMiles++)
-            } else if (Resposta33) {
-                var valorGwen = Number(localStorage.getItem('gwen'));
-                localStorage.setItem("gwen", valorGwen++)
-            } else if (Resposta34) {
-                var valorPeter = Number(localStorage.getItem('peter'));
-                localStorage.setItem("peter", valorPeter++)
-            } else if (Resposta36) {
-                var valorNoir = Number(localStorage.getItem('noir'));
-                localStorage.setItem("noir", valorNoir++)
-            } else if (Resposta31) {
-                var valorPeni = Number(localStorage.getItem('peni'));
-                localStorage.setItem("peni", valorPeni++)
-            } else {
-                var valorPorker = Number(localStorage.getItem('porker'));
-                localStorage.setItem("porker", valorPorker++)
-            }
-
-
-        }
-
-
-
-    }
+var miles = Number(localStorage.getItem('miles'));
+var gwen = Number(localStorage.getItem('gwen'));
+var peter = Number(localStorage.getItem('peter'));
+var peni = Number(localStorage.getItem('peni'));
+var noir = Number(localStorage.getItem('noir'));
+var porker = Number(localStorage.getItem('porker'));
 
     if (miles > peter && miles > gwen && miles > porker && miles > noir && miles > peni) {
 
