c708452f2fc8c369458f20e4680c9de1b2e0b480
jonatao37 <jonathan.almeida@sptech.school>
2023-06-01 22:44:58 -0300

    Quiz finalizado

--- Site institucional/web-data-viz-main/site/public/Itens_site/quiz.js
+++ Site institucional/web-data-viz-main/site/public/Itens_site/quiz.js
@@ -46,7 +46,7 @@ function responder (pergunta, resposta) {
 
     }
     
-    if(pergunta < 6){
+    if(pergunta < 12){
     var pagina = `pergunta${pergunta + 1}.html`;
     window.location.href = pagina;
 
@@ -71,32 +71,68 @@ var porker = Number(localStorage.getItem('porker'));
 
     if (miles > peter && miles > gwen && miles > porker && miles > noir && miles > peni) {
 
-        msg_aranha.innerHTML = `Você é o miles`
+        msg_aranha.innerHTML = `
+        
+        <img src="../assets/img/gifmilesquiz.gif" alt="">
+
+        <h1>Miles Morales</h1>
+        
+        Você é um jovem e talentoso Homem-Aranha que está aprendendo a lidar com seus poderes e responsabilidades e que tem um bom coração.`
 
 
     }else if (peter > miles && peter > gwen && peter > porker && peter > noir && peter > peni) {
 
-        msg_aranha.innerHTML = `Você é o Peter B. Parker`
+        msg_aranha.innerHTML = `
+        
+        <img src="../assets/img/gifpeter.gif" alt="">
+
+        <h1>Peter B. Parker</h1>
+        
+        O Homem-Aranha experiente, habilidoso e bastante destemido de outra dimensão, e meio fudido.`
 
 
     }else if (gwen > peter && gwen > miles && gwen > porker && gwen > noir && gwen > peni) {
 
-        msg_aranha.innerHTML = `Você é a Gwen Stacy`
+        msg_aranha.innerHTML = `
+        
+        <img src="../assets/img/gifgwen.gif" alt="">
+
+        <h1>Gwen Stacy</h1>
+        
+        Neste universo, quem foi picada pela aranha e ganhou os poderes não foi Peter Parker, e sim Gwen Stacy. Outra diferença do universo principal foi como ela decidiu ser uma super-heroína. Gwen apenas decidiu assumir a responsabilidade de ser a Mulher-Aranha após Peter Parker morrer tentando ser especial como ela.`
 
 
     }else if (noir > peter && noir > gwen && noir > porker && noir > miles && noir > peni) {
 
-        msg_aranha.innerHTML = `Você é o Homem aranha Noir`
+        msg_aranha.innerHTML = `
+        
+        <img src="../assets/img/gifnoir.gif" alt="">
+
+        <h1>Homem-aranha Noir</h1>
+        
+        O Homem-Aranha da década de 1930 com uma abordagem única e um senso de humor sarcástico, e uma sabedoria e experiencia um pouco mais superior.`
 
 
     }else if (peni > peter && peni > gwen && peni > porker && peni > noir && peni > miles) {
 
-        msg_aranha.innerHTML = `Você é a Peni Parker`
+        msg_aranha.innerHTML =  `
+        
+        <img src="../assets/img/gifpeni.gif" alt="">
+
+        <h1>Peni Parker</h1>
+        
+        Você é animada(o) e divertida(0), gosta de fazer coisas usando bastante a criatividade e é muito inteligente.`
 
 
     }else {
 
-        msg_aranha.innerHTML = `Você é o Peter Porker`
+        msg_aranha.innerHTML =  `
+        
+        <img src="../assets/img/gifporker.gif" alt="">
+
+        <h1>Peter Porker</h1>
+        
+        o Homem-Aranha porco antropomórfico com suas habilidades peculiares e seu estilo descontraído.`
 
 
     }
